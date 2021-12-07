9ed469dc29e6694d3f0f5267135521023266b5ff
tjsingh <taranjeets120@gmail.com>
2021-12-07 18:41:05 +0530

    created new app users for registrations

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Post
+
+def home(request):
+    context={
+        'posts':Post.objects.all()
+    }
+    
+    return render(request, 'blog/home.htm',context)
+
+def about(request):
+    return render(request, 'blog/about.htm',{'name':'About'})
