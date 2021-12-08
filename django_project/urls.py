11f1a3de3646d94b65a58a25415c8b42e895b21c
tjsingh <taranjeets120@gmail.com>
2021-12-08 19:02:59 +0530

    register form completed using crispy-form

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,8 +1,12 @@
-
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path,include
-
+from django.views.generic.base import TemplateView
+from users import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/',user_views.register,name='register'),
+    path('login/',auth_views.LoginView.as_view(template_name='users/login.htm'),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.htm'),name='logout'),
     path('', include('blog.urls'))
 ]
